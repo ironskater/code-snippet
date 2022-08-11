8a22b64097473b0d4754573e79980355f4cc2355
Hyde Liao <kcritkernel@gmail.com>
2022-08-12 01:12:31 +0800

    [java] update spring-mvc home_controller example

---
+++ java/spring/mvc/home_controller/src/main/java/codesnippet/spring/mvc/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package codesnippet.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController
+{
+    @GetMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    @GetMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+}
---
+++ java/spring/mvc/home_controller/src/main/webapp/WEB-INF/views/helloworld.jsp
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        hello world of springboot + jsp
+        <br><br>
+        Student name: ${param.studentName}
+    </body>
+</html>
