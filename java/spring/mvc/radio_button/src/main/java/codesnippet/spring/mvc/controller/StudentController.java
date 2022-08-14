c3152dc18fbf16dc875a8f636a77f80105e69cff
Hyde Liao <kcritkernel@gmail.com>
2022-08-15 00:12:01 +0800

    [java] rm old spring-mvc examples

---
+++ java/spring/mvc/radio_button/src/main/java/codesnippet/spring/mvc/controller/StudentController.java
@@ -0,0 +1,42 @@
+package codesnippet.spring.mvc.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import codesnippet.spring.mvc.model.Student;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public StudentController() {
+        LOGGER.info("====================================== init {}", this.getClass().getName());
+    }
+
+	@RequestMapping("/showForm")
+	public String
+		showForm(Model model)
+	{
+		Student student = new Student();
+
+		// add the student attribute to the model
+		// so that the student-form jsp can use it
+		model.addAttribute("student", student);
+
+		return "student-form";
+	}
+
+	@RequestMapping("processForm")
+	public String
+		processForm(@ModelAttribute("student") Student student)
+	{
+		System.out.println("the student: " + student.getFirstName() + " " + student.getLastName());
+		return "student-confirmation";
+	}
+}
