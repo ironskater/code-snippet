24a146987f4834ef0a73f7e4d16bf9297e33a170
Hyde Liao <kcritkernel@gmail.com>
2022-09-03 00:09:47 +0800

    [java] update spring-security in_memory example

---
+++ java/spring/security/in_memory/src/main/java/codesnippet/spring/security/controller/LoginController.java
@@ -0,0 +1,13 @@
+package codesnippet.spring.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/custom-loginPage")
+    public String customLoginPage() {
+        // return "plain-login";
+        return "fancy-login";
+    }
+}
