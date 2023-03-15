971689c3fd7eb915880b57cb6764050f53e7f087
hydeliao <hyde.liao@mexc.com>
2023-03-15 23:10:53 +0800

    refactor(java, rest): Rm basic

--- java/spring/rest/basic/book.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS book;
-
-CREATE TABLE book (
-	book_id int NOT NULL GENERATED ALWAYS AS IDENTITY,
-	"name" varchar(255) NULL,
-	author varchar(255) NULL,
-	CONSTRAINT book_pk PRIMARY key(book_id)
-);
